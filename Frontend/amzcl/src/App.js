283fd45c332bcceda15cbcf94dfb6a3b4cf90b2a
NhutDuy <duynhut75@gmail.com>
2025-02-08 17:40:51 +0700

    Integrating Dashboard Theme from hackstarsj/ReactMUIDashboard

--- Frontend/amzcl/src/App.js
+++ Frontend/amzcl/src/App.js
@@ -1,27 +1,46 @@
-
 import './App.css';
-import Layout from './utils/Layout';
-import AuthScreen from './pages/AuthScreen';
+import Layout from './layout/layout';
+import Auth from './pages/Auth';
 import Home from './pages/Home';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'; // ✅ Đúng
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import ProtectedRoute from './utils/ProtectedRoute';
+import { ToastContainer } from 'react-toastify';
 
+const sidebarItems = [
+  { name: "Home", link: "/home", icon: "home" },
+  { name: "Products", link: "/products", icon: "products" },
+  { name: "Categories", icon: "categories", children: [
+      { name: "All Categories", link: "/categories" },
+      { name: "Add Categories", link: "/categories/add" }
+    ]
+  },
+  { name: "Orders", link: "/orders", icon: "orders" },
+  { name: "Users", link: "/users", icon: "users" },
+  { name: "Settings", link: "/settings", icon: "settings" },
+];
 
 const router = createBrowserRouter([
-  { path: "/", element: <AuthScreen /> },
-  { path: "/home", element: <ProtectedRoute element={<Home/>}/>},
+  { path: "/auth", element: <Auth /> },
+  {
+    path: "/",
+    element: <Layout sidebarList={sidebarItems} />,
+    children: [
+      {
+        path: "home", 
+        element: <ProtectedRoute><Home /></ProtectedRoute>
+      }
+    ]
+  },
 ]);
 
 function App() {
   return (
-    
-    <Layout>
-      <RouterProvider router={router}/>
-
-    </Layout>
-    
-  )
+    <>
+      <RouterProvider router={router} />
+      <ToastContainer position="bottom-right" autoClose={3000} hideProgressBar={false} style={{ marginBottom: '30px' }} />
+    </>
+  );
 }
 
 export default App;
